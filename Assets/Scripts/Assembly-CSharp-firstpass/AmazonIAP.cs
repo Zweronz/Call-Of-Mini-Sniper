c999cbc173d64ccde005ecb6d10cca9d090cc0c4
EnderPurrson <enderlord9990@gmail.com>
2024-11-04 19:21:57 -0500

    removed android plugin stuff

--- Assets/Scripts/Assembly-CSharp-firstpass/AmazonIAP.cs
+++ Assets/Scripts/Assembly-CSharp-firstpass/AmazonIAP.cs
@@ -7,38 +7,18 @@ public class AmazonIAP
 
 	static AmazonIAP()
 	{
-		if (Application.platform != RuntimePlatform.Android)
-		{
-			return;
-		}
-		using (AndroidJavaClass androidJavaClass = new AndroidJavaClass("com.prime31.AmazonIAPPlugin"))
-		{
-			_plugin = androidJavaClass.CallStatic<AndroidJavaObject>("instance", new object[0]);
-		}
+		return;
 	}
 
 	public static void initiateItemDataRequest(string[] items)
 	{
-		if (Application.platform == RuntimePlatform.Android)
-		{
-			IntPtr methodID = AndroidJNI.GetMethodID(_plugin.GetRawClass(), "initiateItemDataRequest", "([Ljava/lang/String;)V");
-			AndroidJNI.CallVoidMethod(_plugin.GetRawObject(), methodID, AndroidJNIHelper.CreateJNIArgArray(new object[1] { items }));
-		}
 	}
 
 	public static void initiatePurchaseRequest(string sku)
 	{
-		if (Application.platform == RuntimePlatform.Android)
-		{
-			_plugin.Call("initiatePurchaseRequest", sku);
-		}
 	}
 
 	public static void initiateGetUserIdRequest()
 	{
-		if (Application.platform == RuntimePlatform.Android)
-		{
-			_plugin.Call("initiateGetUserIdRequest");
-		}
 	}
 }
